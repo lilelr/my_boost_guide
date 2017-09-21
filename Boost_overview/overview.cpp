5e5ed7fbd532db6e23ade2cbeeede6b796fb2bbb
Lele Li <lilelr@163.com>
2017-09-21 15:48:39 +0800

    hello

---
+++ Boost_overview/overview.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <boost/version.hpp>
+#include<boost/config.hpp>
+using namespace std;
+
+int main() {
+    cout<<BOOST_VERSION<<endl;
+    cout<<BOOST_LIB_VERSION<<endl;
+    cout<<BOOST_PLATFORM<<endl;
+    cout<<BOOST_COMPILER<<endl;
+    cout<<BOOST_STDLIB<<endl;
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ build/CMakeFiles/my_boost_guide.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_boost_guide.dir/main.cpp.o"
+  "my_boost_guide.pdb"
+  "my_boost_guide"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_boost_guide.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
